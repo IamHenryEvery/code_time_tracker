ce8f749e8fabbd021b65a4eff502c9a8b002d74d
nikita grishin <nikitagr1205@yandex.ru>
2025-03-20 15:31:52 +0300

    added functions to show proccesses with windows and wrote docstrings

--- google_calendar_script.py
+++ google_calendar_script.py
@@ -1,30 +1,90 @@
 import os.path
+import time
 
 import arrow
+import psutil
 import win32gui
+import win32process
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
 
-def get_active_window_title():
-    hwnd = win32gui.GetForegroundWindow()
-    title = win32gui.GetWindowText(hwnd)
-    return title
+def enum_windows_callback(hwnd, processes_with_windows: set):
+    """Колбек функция для добавления названия процессов с окнами в множество
+
+    Args:
+        hwnd: Идентификатор окна
+        processes_with_windows (set): Множество процессов с открытыми окнами
+    """
+    try:
+        _, pid = win32process.GetWindowThreadProcessId(hwnd)
+        process_name = get_process_name_by_pid(pid)
+        if process_name:
+            processes_with_windows.add(process_name)
+    except (
+        psutil.NoSuchProcess,
+        psutil.AccessDenied,
+        psutil.ZombieProcess,
+    ):
+        pass
 
 
-def is_vscode_focused():
-    title = get_active_window_title()
-    return "Visual Studio Code" in title
+def get_processes_with_windows() -> set[str]:
+    """Функция для создания множества названий процессов с открытыми окнами
+
+    Returns:
+        set[str]: Множество процессов с открытыми окнами
+    """
+    processes_with_windows = set()
+    win32gui.EnumWindows(enum_windows_callback, processes_with_windows)
+    return processes_with_windows
+
+
+def get_process_name_by_pid(pid: int) -> str:
+    """Фукнция для определения названия процесса по его идентификатору
+
+    Args:
+        pid (int): Идентификатор процесса
+
+    Returns:
+        str: Название процесса
+    """
+    try:
+        process = psutil.Process(pid)
+        return process.name().lower()
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+        return ""
+
+
+def is_programming(processes_with_windows: set[str]) -> bool:
+    """Фукнция, которая определит, открыт ли VsCode или PyCharm
+
+    Args:
+        processes_with_windows (set[str]): Множество процессов с открытыми
+        окнами
+
+    Returns:
+        bool: True, если открыт VsCode или PyCharm
+        False, если нет
+    """
+    return (
+        "code.exe" in processes_with_windows
+        or "pycharm64.exe" in processes_with_windows
+    )
 
 
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 
-#Todo Добавить комментарии
-def main():
+def connect_to_calendar():
+    """Функция для подключения к Google календарю, используя учетные данные
+    из файла credentials.json и/или токен из файла token.json
+
+    Returns:
+        service: Сервис для поключения к API Google календаря
+    """
     creds = None
 
     if os.path.exists("token.json"):
@@ -40,23 +100,37 @@ def main():
         with open("token.json", "w") as token:
             token.write(creds.to_json())
 
-    try:
-        service = build("calendar", "v3", credentials=creds)
-        start = arrow.now().shift(hours=-5).isoformat()
-        end = arrow.now().shift(hours=-2).isoformat()
-        #TODO Добавить основную логику добавления события
-        event = {
-            "summary": "Created via API event",
-            "start": {"dateTime": start},
-            "end": {"dateTime": end},
-        }
-        event = (
-            service.events().insert(calendarId="primary", body=event).execute()
-        )
-        print(f"Event created: {event.get('htmlLink')}")
-    except HttpError as error:
-        print(f"An error occurred: {error}")
+    service = build("calendar", "v3", credentials=creds)
+    return service
+
+
+
+def main(service):
+    """Главная функция для добавления событий в календарь
+
+    Args:
+        service: Сервис для поключения к API Google календаря
+    """
+    start = arrow.now().isoformat()
+    while True:
+        # TODO Добавить обработку случая с повторным открытием окна
+        if not is_programming(get_processes_with_windows()):
+            end = arrow.now().isoformat()
+            event = {
+                "summary": "Программирование",
+                "start": {"dateTime": start},
+                "end": {"dateTime": end},
+            }
+            event = (
+                service.events()
+                .insert(calendarId="primary", body=event)
+                .execute()
+            )
+            print(f"Event created: {event.get('htmlLink')}")
+
+        time.sleep(60)
 
 
 if __name__ == "__main__":
-    main()
+    service = connect_to_calendar()
+    main(service)
